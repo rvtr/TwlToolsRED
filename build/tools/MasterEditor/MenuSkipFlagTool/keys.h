2a10fe9707078b40588185f7f205af764eb41216
nishikawa_takeshi <nishikawa_takeshi@randommeaninglesscharacters.com>
2009-06-19 08:31:40 +0000

    DSiメニュースキップフラグ立てツール:初版。ドキュメントはdocs/MasterEditorTWL以下を参照。
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlToolsRED@293 7061adef-622a-194b-ae81-725974e89856

---
+++ build/tools/MasterEditor/MenuSkipFlagTool/keys.h
@@ -0,0 +1,14 @@
+// 署名用の鍵データの宣言
+
+#include <twl/types.h>
+
+// 開発機用ROMヘッダ署名鍵
+extern const u8 g_devPubKey_DER_User[ 0xa2 ];
+extern const u8 g_devPrivKey_DER_User[ 0x263 ];
+extern const u8 g_devPubKey_DER_System[ 0xa2 ];
+extern const u8 g_devPrivKey_DER_System[ 0x263 ];
+extern const u8 g_devPubKey_DER_Secure[ 0xa2 ];
+extern const u8 g_devPrivKey_DER_Secure[ 0x263 ];
+extern const u8 g_devPubKey_DER_Launcher[ 0xa2 ];
+extern const u8 g_devPrivKey_DER_Launcher[ 0x263 ];
+

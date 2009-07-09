59fb67c68c4c4bab6c35cc7e1788c38ddeba4061
nishikawa_takeshi <nishikawa_takeshi@randommeaninglesscharacters.com>
2009-07-09 05:33:26 +0000

    マスタエディタ出力チェッカ:ファイル削除用バッチファイルを追加。
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlToolsRED@373 7061adef-622a-194b-ae81-725974e89856

---
+++ build/tools/MasterEditor/MasterEditorTWLChecker/rm.bat
@@ -0,0 +1,12 @@
+set my_dir=%~d0%~p0
+
+del "%my_dir%TKGUJ00.SRL"
+del "%my_dir%TKGUJ00_MIDDLEWARE.XML"
+del "%my_dir%TKGUJ00_MIDDLEWARE.HTML"
+del "%my_dir%TKGUJ00_SHEET.XML"
+
+del "%my_dir%T0CZA00.SRL"
+del "%my_dir%T0CZA00_MIDDLEWARE.XML"
+del "%my_dir%T0CZA00_MIDDLEWARE.HTML"
+del "%my_dir%T0CZA00_SHEET.XML"
+
